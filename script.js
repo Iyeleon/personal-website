f7dba488323dadc8a11a087e36468d0cb0afadc4
DamiAkinniyi <oluwadamilola.akinniyi@students.iaac.net>
2024-06-02 17:53:26 -0500

    Modified Scroll

--- script.js
+++ script.js
@@ -35,6 +35,9 @@
 
 // navigate to a section
 let ghost = document.getElementById('ghost')
+let blogGhost = document.getElementById('blog')
+let landing = document.getElementById('landing')
+console.log(blogGhost)
 menuLinks = document.querySelectorAll("nav ul li a");
 menuLinks.forEach(link => {
     link.addEventListener('click', function(event){
@@ -47,35 +50,52 @@ menuLinks.forEach(link => {
         const targetPage = document.getElementById(targetPageID)
         // activate target page
         console.log(this.getAttribute("href"))
-        console.log(this)
+        console.log(targetPageID)
         targetPage.classList.remove('inactive')
         targetPage.classList.add('active')
         if (targetPageID == 'projects'){            
             ghost.classList.add('active')
             ghost.classList.remove('inactive') 
             if (currActive.includes('projects')){
-                targetPage.scrollIntoView({block: "nearest", behavior: "auto" });
-                window.scrollBy(0, -100);
+                console.log('you should scroll')
+                targetPage.scrollIntoView({block: "nearest", behavior: "smooth" });
+                console.log('scrolling into view')
+                landing.setAttribute('opacity', "0.5")
+                console.log('setting opacity')
+                // window.scrollBy(0, -100);
             }
             else{
                 // window.location = '#'+targetPageID
             }
         }
+        else if(targetPageID == 'blog-stories'){
+            blogGhost.classList.add('active')
+            blogGhost.classList.remove('inactive')
+            window.location = `#${targetPageID}`
+            console.log(window.location)
+            // window.scrollTo(0, 0)
+        }
         else if (targetPageID == 'contact'){
             // Do something
-
         }
         else{
+            // ghost.classList.add('inactive')
+            // ghost.classList.remove('active') 
+            // targetPage.scrollIntoView({ behavior: "smooth" })
+            // window.scrollBy(0, -100);
+            window.location = `#${targetPageID}`
+        }
+        if (targetPageID != 'projects'){
             ghost.classList.add('inactive')
             ghost.classList.remove('active') 
-            targetPage.scrollIntoView({ behavior: "smooth" })
-            window.scrollBy(0, -100);
-            // window.location = '#'+targetPageID
+            window.location = `#${targetPageID}`
+            console.log('location moved')
         }
         
         console.log(currActive)
         if (!currActive.includes(targetPageID)){
             currActive.forEach(ele => {
+                console.log('hiding: ', ele)
                 const hidden = document.getElementById(ele);
                 hidden.classList.remove('active')
                 hidden.classList.add('inactive')
@@ -92,4 +112,20 @@ menuLinks.forEach(link => {
         // const targetID = this.getAttribute("href").substring(1)
 
     })
+})
+
+let blogButton = document.getElementById("read-stories")
+let blogLanding = document.getElementById("blog-landing") 
+blogButton.addEventListener('click', function(event){
+    let currActive = ["ghost"]
+    currActive.forEach(ele => {
+        console.log('hiding: ', ele)
+        const hidden = document.getElementById(ele);
+        hidden.classList.remove('active')
+        hidden.classList.add('inactive')
+    })
+    console.log(blogGhost)
+    blogGhost.classList.add('active')
+    blogGhost.classList.remove('inactive')
+
 })
